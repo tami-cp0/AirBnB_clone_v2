2f32aad55aaf5da99c215112edeedd5839f743d8
ayo <findtamilore@gmail.com>
2024-02-18 16:56:12 +0100

    db for test setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on the database hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
