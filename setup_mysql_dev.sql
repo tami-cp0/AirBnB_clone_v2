83fce04e56fbe6fe9fc3bcc264c91e0c52dab29f
ayo <findtamilore@gmail.com>
2024-02-18 16:42:50 +0100

    database set up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,16 @@
--- Create a MySQL server for the project--
+-- Create a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Generate the hashed password
-SET @hashed_password = PASSWORD('hbnb_dev_pwd');
+-- Set password policy to low temporarily
+SET GLOBAL validate_password.policy = LOW;
 
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Update the user's password in the mysql.user table
-UPDATE mysql.user SET authentication_string = @hashed_password WHERE user = 'hbnb_dev' AND host = 'localhost';
-
+-- Grant all privileges on the database hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply changes
