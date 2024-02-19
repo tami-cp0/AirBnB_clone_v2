4de8033ba2f2c331fb8c17ffdc1368c1aa423fed
ayo <findtamilore@gmail.com>
2024-02-19 20:30:48 +0100

    revert

--- models/user.py
+++ models/user.py
@@ -10,13 +10,17 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+<<<<<<< HEAD
     __tablename__ = 'users'
     places = relationship(
         "Place", backref="user",
         cascade="all, delete, delete-orphan"
     )
+=======
+    __tablename__ = "users"
+>>>>>>> parent of f2bf6e1 (changed table records)
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
