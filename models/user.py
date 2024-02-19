960b453defe80a342a08203518361847320f3c96
ayo <findtamilore@gmail.com>
2024-02-19 20:04:22 +0100

    changed table records

--- models/user.py
+++ models/user.py
@@ -12,5 +12,5 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
