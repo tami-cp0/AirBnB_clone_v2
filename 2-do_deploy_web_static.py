00a3ce9be5c392ec8096ab4211b83dc83218b4e2
tami-cp0 <findtamilore>
2024-03-07 03:43:28 +0100

    deployment complete v5

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,42 +11,36 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    archive_filename = os.path.basename(archive_path).split(".")[0]
-    tar_file = os.path.basename(archive_path)
-    # Upload the archive to the remote server
-    if put(archive_path, f"/tmp/{tar_file}").failed:
-        return False
-    # remove file
-    if run(f"rm -rf /data/web_static/releases/{archive_filename}/").failed:
-        return False
-    # Create directory for extraction
-    if run(f"mkdir -p /data/web_static/releases/{archive_filename}/").failed:
-        return False
+    try:
+        archive_filename = os.path.basename(archive_path).split(".")[0]
+        tar_file = os.path.basename(archive_path)
+        # Upload the archive to the remote server
+        put(archive_path, f"/tmp/{tar_file}")
+        # remove file
+        run(f"rm -rf /data/web_static/releases/{archive_filename}/")
+        # Create directory for extraction
+        run(f"mkdir -p /data/web_static/releases/{archive_filename}/")
 
-    # Extract the archive
-    if run(f"tar -xzvf /tmp/{tar_file} -C "
-        f"/data/web_static/releases/{archive_filename}/").failed:
-        return False
+        # Extract the archive
+        run(f"tar -xzvf /tmp/{tar_file} -C "
+            f"/data/web_static/releases/{archive_filename}/")
 
-    # Remove the temporary archive file
-    if run(f"rm /tmp/{tar_file}").failed:
-        return False
+        # Remove the temporary archive file
+        run(f"rm /tmp/{tar_file}")
 
-    # Move files to the appropriate location
-    if run(f"mv /data/web_static/releases/{archive_filename}"
-        f"/web_static/* /data/web_static/releases/{archive_filename}/").failed:
-        return False
+        # Move files to the appropriate location
+        run(f"mv /data/web_static/releases/{archive_filename}"
+            f"/web_static/* /data/web_static/releases/{archive_filename}/")
 
-    # Remove the now empty web_static directory
-    if run(f"rm -rf /data/web_static/releases/{archive_filename}/web_static").failed:
-        return False
+        # Remove the now empty web_static directory
+        run(f"rm -rf /data/web_static/releases/{archive_filename}/web_static")
 
-    # remove the current symbolic link
-    if run("rm -rf /data/web_static/current").failed:
-        return False
+        # remove the current symbolic link
+        run("rm -rf /data/web_static/current")
 
-    # Update symbolic link
-    if run(f"ln -s /data/web_static/releases/"
-        f"{archive_filename}/ /data/web_static/current").failed:
+        # Update symbolic link
+        run(f"ln -s /data/web_static/releases/"
+            f"{archive_filename}/ /data/web_static/current")
+        return True
+    except Exception as e:
         return False
-    return True
