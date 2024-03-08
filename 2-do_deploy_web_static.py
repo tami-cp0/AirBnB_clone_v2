59a9589f173b500373521bd6429d362d9522cd2b
tami-cp0 <findtamilore>
2024-03-08 15:41:29 +0100

    update v15

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+#  Fabric script that distributes an archive to your web servers
+from fabric.api import *
+from datetime import datetime
+import os
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+
+env.user = 'ubuntu'
+env.hosts = ['54.157.167.117', '54.160.75.58']
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    now = datetime.utcnow()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{timestamp}.tgz"
+    archive_path = f"versions/{archive_name}"
+
+    if not os.path.exists("versions"):
+        if local("mkdir versions").failed:
+            return None
+
+    if local(f"tar -cvzf {archive_path} web_static").succeeded:
+        return archive_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -17,33 +35,48 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    filename = os.path.basename(archive_path)
+    # without extension
+    raw_name = filename.split(".")[0]
+
+    # Upload the archive to the remote server
+    if put(archive_path, f"/tmp/").failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    # Create directory for extraction
+    if run(f"mkdir -p /data/web_static/releases/{raw_name}").failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    # Extract the archive
+    if run(f"tar -xzf /tmp/{filename} -C "
+           f"/data/web_static/releases/{raw_name}/").failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+
+    # Remove the temporary archive file
+    if run(f"rm -rf /tmp/{filename}").failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+
+    # Move files to the appropriate location
+    if run(f"mv /data/web_static/releases/{raw_name}/web_s"
+           f"tatic/* /data/web_static/releases/{raw_name}/").failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+
+    # Remove the now empty web_static directory
+    if run(f"rm -rf /data/web_static/releases/"
+           f"{filename}/web_static").failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+
+    # remove the symbolic link
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+
+    # Update symbolic link
+    if run(f"ln -s /data/web_static/releases/"
+           f"{raw_name} /data/web_static/current").failed:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+
+    print("New version deployed!")
     return True
