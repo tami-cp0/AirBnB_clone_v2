b86acbc7e10c922ae87ee89cd633a4e1dc3b1dc3
tami-cp0 <findtamilore>
2024-03-09 00:01:07 +0100

    Keep it clean! test

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,40 +2,26 @@
 """
 This file contains a script that deletes out-of-date archives
 """
-from fabric.api import env, run, local
-import os
+from fabric.api import env, run
+from os import listdir, remove
+from os.path import isfile, join
+
+
 env.user = 'ubuntu'
 env.hosts = ['54.157.167.117', '54.160.75.58']
 
 
 def do_clean(number=0):
-    """
-    Deletes out-of-date archives from the versions folder.
-
-    Args:
-        number (int, optional): The number of recent archives to keep.
-            Defaults to 0, which removes all but the most recent.
-
-    Returns:
-        None
-    """
-    local_files = os.listdir("versions")
-
-    # store remote files
-    # number_remote_files = run('ls -1 /data/web_static/releases/ |'
-    #                           ' grep "web_static_*" | wc -l')
+    """Deletes out-of-date archives"""
     number = int(number)
     if number < 1:
         number = 1
 
-    if len(local_files) > number:
-        local(f"ls -t versions/ | grep 'web_static_*' |"
-              f" tail -n +{number + 1} "
-              "| xargs -I % rm versions/%")
-
-    # run(f"ls -t /data/web_static/releases/ | grep 'web_static_*' |"
-    #     f"tail -n +{int(number) + 1} |"
-    #     "xargs -I % rm -rf /data/web_static/releases/%")
+    # Delete unnecessary archives in the versions folder
+    files = [f for f in listdir('versions') if isfile(join('versions', f))]
+    files.sort(reverse=True)
+    for file in files[number:]:
+        remove(join('versions', file))
 
     # Delete unnecessary archives on the servers releases folder
     cmd_list = 'ls -t /data/web_static/releases'
