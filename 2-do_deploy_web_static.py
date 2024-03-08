21dca3289ae1dae72481051a04cba5819f49c95b
tami-cp0 <findtamilore>
2024-03-08 15:05:23 +0100

    update v12

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,30 +30,44 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    try:
-        archive_filename = os.path.basename(archive_path).split(".")[0]
-        tar_file = os.path.basename(archive_path)
-        # Upload the archive to the remote server
-        put(archive_path, f"/tmp/{tar_file}")
-        # remove previous archive file
-        run(f"rm -rf /data/web_static/releases/web_static_*")
-        # Create directory for extraction
-        run(f"mkdir -p /data/web_static/releases/{archive_filename}")
-        # Extract the archive
-        run(f"tar -xzvf /tmp/{tar_file} -C "
-            f"/data/web_static/releases/{archive_filename}/")
-        # Remove the temporary archive file
-        run(f"rm /tmp/{tar_file}")
-        # Move files to the appropriate location
-        run(f"cp /data/web_static/releases/{archive_filename}"
-            f"/web_static/* /data/web_static/releases/{archive_filename}/")
-        # Remove the now empty web_static directory
-        run(f"rm -rf /data/web_static/releases/"
-            f"{archive_filename}/web_static")
-        # Update symbolic link
-        run(f"ln -sf /data/web_static/releases/"
-            f"{archive_filename} /data/web_static/current")
-        print("New version deployed!")
-        return True
-    except Exception as e:
+    archive_filename = os.path.basename(archive_path).split(".")[0]
+    tar_file = os.path.basename(archive_path)
+
+    # Upload the archive to the remote server
+    if put(archive_path, f"/tmp/{tar_file}").failed:
         return False
+
+    # remove previous archive file
+    if run(f"rm -rf /data/web_static/releases/web_static_*").failed:
+        return False
+
+    # Create directory for extraction
+    if run(f"mkdir -p /data/web_static/releases/{archive_filename}").failed:
+        return False
+
+    # Extract the archive
+    if run(f"tar -xzvf /tmp/{tar_file} -C "
+           f"/data/web_static/releases/{archive_filename}/").failed:
+        return False
+
+    # Remove the temporary archive file
+    if run(f"rm /tmp/{tar_file}").failed:
+        return False
+
+    # Move files to the appropriate location
+    if run(f"mv /data/web_static/releases/{archive_filename}/web_s"
+           f"tatic/* /data/web_static/releases/{archive_filename}/").failed:
+        return False
+
+    # Remove the now empty web_static directory
+    if run(f"rm -rf /data/web_static/releases/"
+           f"{archive_filename}/web_static").failed:
+        return False
+
+    # Update symbolic link
+    if run(f"ln -sf /data/web_static/releases/"
+           f"{archive_filename} /data/web_static/current").failed:
+        return False
+
+    print("New version deployed!")
+    return True
