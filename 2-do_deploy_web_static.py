3f1cf7f51fe789f335fec523e296a6d297f8b36f
tami-cp0 <findtamilore>
2024-03-08 14:20:59 +0100

    update v11

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,40 +8,6 @@ env.hosts = ['54.157.167.117', '54.160.75.58']
 env.key_filename = '~/.ssh/id_rsa'
 
 
-def do_deploy(archive_path):
-    """Deploys static releases to servers"""
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        archive_filename = os.path.basename(archive_path).split(".")[0]
-        tar_file = os.path.basename(archive_path)
-        # Upload the archive to the remote server
-        put(archive_path, f"/tmp/{tar_file}")
-        # remove file
-        run(f"rm -rf /data/web_static/releases/web_static_*")
-        # Create directory for extraction
-        run(f"mkdir -p /data/web_static/releases/{archive_filename}")
-        # Extract the archive
-        run(f"tar -xzvf /tmp/{tar_file} -C "
-            f"/data/web_static/releases/{archive_filename}/")
-        # Remove the temporary archive file
-        run(f"rm /tmp/{tar_file}")
-        # Move files to the appropriate location
-        run(f"mv /data/web_static/releases/{archive_filename}"
-            f"/web_static/* /data/web_static/releases/{archive_filename}/")
-        # Remove the now empty web_static directory
-        run(f"rm -rf /data/web_static/releases/"
-            f"{archive_filename}/web_static")
-        # Update symbolic link
-        run(f"ln -sf /data/web_static/releases/"
-            f"{archive_filename} /data/web_static/current")
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        return False
-
-
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     now = datetime.utcnow()
@@ -57,3 +23,37 @@ def do_pack():
         return archive_path
     else:
         return None
+
+
+def do_deploy(archive_path):
+    """Deploys static releases to servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        archive_filename = os.path.basename(archive_path).split(".")[0]
+        tar_file = os.path.basename(archive_path)
+        # Upload the archive to the remote server
+        put(archive_path, f"/tmp/{tar_file}")
+        # remove previous archive file
+        run(f"rm -rf /data/web_static/releases/web_static_*")
+        # Create directory for extraction
+        run(f"mkdir -p /data/web_static/releases/{archive_filename}")
+        # Extract the archive
+        run(f"tar -xzvf /tmp/{tar_file} -C "
+            f"/data/web_static/releases/{archive_filename}/")
+        # Remove the temporary archive file
+        run(f"rm /tmp/{tar_file}")
+        # Move files to the appropriate location
+        run(f"cp /data/web_static/releases/{archive_filename}"
+            f"/web_static/* /data/web_static/releases/{archive_filename}/")
+        # Remove the now empty web_static directory
+        run(f"rm -rf /data/web_static/releases/"
+            f"{archive_filename}/web_static")
+        # Update symbolic link
+        run(f"ln -sf /data/web_static/releases/"
+            f"{archive_filename} /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        return False
