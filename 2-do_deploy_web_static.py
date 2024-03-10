cd218c8bb70416728b7b5ffca061db3ab07b4265
tami-cp0 <findtamilore>
2024-03-10 11:37:41 +0100

    final

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,64 @@
 #!/usr/bin/python3
+""" These methods aid in deploying the web_static directory
+to the remote servers
 """
-This file contains a script that distribute an archive to my web servers
-using the function do_deploy
-"""
-import os
 from fabric.api import *
+from os import path
 from datetime import datetime
 
-
-env.user = 'ubuntu'
-env.hosts = ['54.157.167.117', '54.160.75.58']
-
+# set the server hosts for web-01 and web-02
+env.hosts = [
+    "52.87.254.150",
+    "52.86.39.247",
+]
+# set the username
+env.user = "ubuntu"
 
 def do_pack():
-    """This function generates a .tgz archive"""
-    local('mkdir -p versions')
-
-    # generate the .tgz archive
-    time = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_{}.tgz'.format(time)
-    result = local('tar -czvf {} web_static'.format(archive_path))
-
-    # if successful return the archive path
-    if result.succeeded:
-        return archive_path
-    else:
+    """This method will pack the web_static dir into a tar.gz
+    for deployinment to remote servers.
+    """
+    if not path.exists(path.dirname("./web_static")):
         return None
+
+    if not path.exists(path.dirname("versions")):
+        try:
+            local("mkdir -p versions")
+        except Exception as e:
+            print(e)
+            return None
+    fname = "web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    local("echo {}".format(fname))
+    local("tar cpfz {} ./web_static".format(fname))
+    local("mv {file} versions/{file}".format(file=fname))
+    return "version/{}".format(fname)
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not os.path.exists(archive_path):
+    """ this method will deploy compressed file
+    then unpack and move the content to is proper destination
+
+    Returns:
+        Bool: True on sucess well Fale
+    """
+    try:
+        open(archive_path)
+    except IOError:
         return False
-
-    print("Deploying new version")
-
-    # extract the filename without extension
-    filename = archive_path.split("/")[-1]
-    name = filename[: -4]
-
-    # define the release directory using the extracted name
-    release_dir = "/data/web_static/releases/{}".format(name)
-
-    # upload and uncompress the archive to a folder
+    split_path = archive_path.split('/')
+    cln_name = split_path[1][0:split_path[1].rfind('.')]
+    dest = '/data/web_static'
     put(archive_path, "/tmp/")
-    run("mkdir -p {}".format(release_dir))
-    r = run("tar -xzf /tmp/{} -C {}".format(filename, release_dir))
+    with cd("/tmp/"):
+        run('tar xpf {}'.format(split_path[1]))
+        run('mv web_static {}/releases/{}'.format(dest, cln_name))
+        run('rm -rf {}'.format(split_path[1]))
 
-    if r.failed:
-        return False
-
-    # copy files into the right directory
-    run("cp -r {}/web_static/* {}".format(release_dir, release_dir))
-    run("rm -rf /tmp/{} {}/web_static".format(filename, release_dir))
-
-    # delete symbolic link from web server
-    run("rm -rf /data/web_static/current")
-
-    # create a symlink to new version of code
-    run("ln -s {} /data/web_static/current".format(release_dir))
-
-    print("New version deployed")
-
+    with cd(dest):
+        run('rm {}/current'.format(dest))
+        run('ln -s {d}/releases/{t} {d}/current'
+            .format(d=dest, t=cln_name))
+    print('New version deployed!')
     return True
